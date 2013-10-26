d62a2ecbfee3c26d4647ec61228758925cab5e6e
nekanat <nekanat.stock@gmail.com>
2013-10-26 21:53:45 +0900

    Divide file

--- fcl/fcl.pyx
+++ fcl/fcl.pyx
@@ -8,20 +8,11 @@ from cython.operator cimport dereference as deref, preincrement as inc, address
 cimport fcl_defs as defs
 import numpy as np
 import transform as tf
+from collision_data import *
 cimport numpy as np
 ctypedef np.float64_t DOUBLE_t
 
-class OBJECT_TYPE:
-    OT_UNKNOWN, OT_BVH, OT_GEOM, OT_OCTREE, OT_COUNT = range(5)
-    def __init__(self):
-        pass
 
-class NODE_TYPE:
-    BV_UNKNOWN, BV_AABB, BV_OBB, BV_RSS, BV_kIOS, BV_OBBRSS, BV_KDOP16, BV_KDOP18, BV_KDOP24,\
-    GEOM_BOX, GEOM_SPHERE, GEOM_CAPSULE, GEOM_CONE, GEOM_CYLINDER, GEOM_CONVEX, GEOM_PLANE,\
-    GEOM_HALFSPACE, GEOM_TRIANGLE, GEOM_OCTREE, NODE_COUNT = range(20)
-    def __init__(self):
-        pass
 
 cdef vec3f_to_tuple(defs.Vec3f vec):
     return (vec[0], vec[1], vec[2])
@@ -247,30 +238,6 @@ class Contact:
         self.normal = [0.0, 0.0, 0.0]
         self.pos = [0.0, 0.0, 0.0]
         self.penetration_depth = 0.0
-
-class CostSource:
-    def __init__(self):
-        self.aabb_min = [0.0, 0.0, 0.0]
-        self.cost_density = 0.0
-        self.total_cost = 0.0
-
-class CollisionResult:
-    def __init__(self):
-        self.contacts = []
-        self.cost_sources = []
-
-class CollisionRequest:
-    def __init__(self,
-                 num_max_contacts = 1,
-                 enable_contact = False,
-                 num_max_cost_sources = 1,
-                 enable_cost = False,
-                 use_approximate_cost = True):
-        self.num_max_contacts = num_max_contacts
-        self.enable_contact = enable_contact
-        self.num_max_cost_sources = num_max_cost_sources
-        self.enable_cost = enable_cost
-        self.use_approximate_cost = use_approximate_cost
 
 cdef class DynamicAABBTreeCollisionManager:
     cdef defs.DynamicAABBTreeCollisionManager *thisptr

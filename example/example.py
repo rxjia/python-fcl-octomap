61d7dc4725fdf5f94964c7b42eea165c4f5b3d83
nekanat <nekanat.stock@gmail.com>
2014-05-02 21:19:24 +0900

    Modify setup.py

--- example/example.py
+++ example/example.py
@@ -4,6 +4,7 @@ objs = [fcl.CollisionObject(fcl.Box(1.0, 2.0, 3.0)),
         fcl.CollisionObject(fcl.Sphere(4.0)),
         fcl.CollisionObject(fcl.Cone(5.0, 6.0))]
 
+# Register objects to DynamicAABBTreeCollisionManager
 manager = fcl.DynamicAABBTreeCollisionManager()
 print "Before resgister: ", manager.size()
 manager.registerObjects(objs)
@@ -11,6 +12,7 @@ print "After register 1 : ", manager.size()
 manager.registerObject(fcl.CollisionObject(fcl.Cylinder(7.0, 8.0)))
 print "After register 2 : ", manager.size()
 
+# Use Callback function
 def cb_func(obj1, obj2, res):
     print "cb_func start"
     ret, res = fcl.collide(obj1, obj2, fcl.CollisionRequest())
@@ -19,7 +21,7 @@ def cb_func(obj1, obj2, res):
 res = fcl.CollisionResult()
 manager.collide(res, cb_func)
 
-
+# Collision calcuration
 ret, result = fcl.collide(fcl.CollisionObject(fcl.Box(1.0, 2.0, 3.0),
                                               transform.Transform(transform.Quaternion(), [10.0, 0.0, 0.0])),
                           fcl.CollisionObject(fcl.Sphere(4.0),
--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
-
+import os
+import sys
 from distutils.core import Extension, setup
 from Cython.Distutils import build_ext
+
+platform_supported = False
+for prefix in ['darwin', 'linux', 'bsd']:
+    if prefix in sys.platform:
+        platform_supported = True
+        include_dirs = [
+            '/usr/include',
+            '/usr/local/include',
+        ]
+        lib_dirs = [
+            '/usr/lib',
+            '/usr/local/lib',
+        ]
+        if 'CPATH' in os.environ:
+            include_dirs += os.environ['CPATH'].split(':')
+        if 'LD_LIBRARY_PATH' in os.environ:
+            lib_dirs += os.environ['LD_LIBRARY_PATH'].split(':')
+        break
+
+if sys.platform == "win32":
+    platform_supported = False
+
+if not platform_supported:
+    raise NotImplementedError(sys.platform)
 
 setup(
     name="fcl",
@@ -10,6 +35,8 @@ setup(
     ext_modules=[Extension(
         "fcl.fcl",
         ["fcl/fcl.pyx"],
+        include_dirs = include_dirs,
+        library_dirs = lib_dirs,
         libraries=[
                 "fcl"
                 ],

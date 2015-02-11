af118db5a6e94fb732da2fede2457ed703f506b0
Jelle Feringa <jelleferinga@gmail.com>
2015-02-11 10:45:51 +0100

    conda build for python-fcl

--- pkg/conda/build.sh
+++ pkg/conda/build.sh
@@ -1,37 +1,14 @@
-#!/bin/bash
-export PREFIX=~/miniconda/
-export NUMPY_INC=/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages/numpy/core/include
-export FCL_PY_DIR=/Library/Python/2.6/site-packages
-#export PY_FCL_DIR
+#!/bin/sh
 
-export HERE=`pwd`
+export NP_INC=`python -c "import numpy; print numpy.get_include()"`
+echo "numpy include directory: " $NP_INC
 
-# UGH: fcl/fcl.cpp:14950:3: warning: conversion from string literal to 'char *' is deprecated
-#      [-Wdeprecated-writable-strings]
+export CXX="clang++"
+export CC="clang"
 
-rm ../fcl/*.cpp
-rm -rf ../build/*
-
-cd ../
-
-echo "compiling cython FCL wrapper"
-#python setup.py build_ext # --include-dirs $PREFIX/include --library-dirs $PREFIX/lib
+python setup.py build_ext --include-dirs $PREFIX/include:$NP_INC --library-dirs $PREFIX/lib
 python setup.py install
-echo "done compiling "
 
-echo "run install name"
-#install_name_tool -change libfcl.dylib @loader_path/../../../libfcl.dylib $FCL_PY_DIR/fcl.so
-#install_name_tool -change libfcl.dylib @loader_path/../../../libfcl.dylib $FCL_PY_DIR/mesh.so
-
-# mehhh, keeps on failing on the already compiled files... strange...
-rm $FCL_PY_DIR/*.pyc
-
-# conflicts with the `fcl` project directory
-# looks for the fcl.so module here and not finding it
-cd ~
-#echo "running fcl unit test"
-#nosetests ~/GIT/python-fcl/test/fcl_unit_test.py
-echo "running mesh unit test"
-nosetests -s ~/GIT/python-fcl/test/mesh_unit_test.py
-#echo "done..."
-cd $HERE
+# TODO: cython has an issue loading .pyc compiled from another directory then where they are stored now...
+# see:
+find $RECIPE_DIR -name "*.pyc" -delete
--- pkg/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nosetests -s ~/GIT/python-fcl/test/fcl_unit_test.py
-nosetests -s ~/GIT/python-fcl/test/mesh_unit_test.py

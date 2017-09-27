7688c2e6cc01ffe6d0533030068e38dc607400b6
Matthew Matl <matthewcmatl@gmail.com>
2017-09-27 13:19:46 -0700

    Remove unused mesh.pyx file

--- fcl/mesh.pyx
+++
@@ -1,7 +0,0 @@
-from libcpp cimport bool
-from cython.operator cimport dereference as deref, preincrement as inc, address
-cimport fcl_defs as defs
-cimport fcl
-cimport numpy as np
-ctypedef np.float64_t DOUBLE_t
-

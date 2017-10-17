f8655278ce4513377d398fad41b6d5a99816b4d4
Michael Dawson-Haggerty <mik3dh@gmail.com>
2017-10-17 19:25:30 -0400

    allow cmake to be called cmake3

--- requirements/build.bash
+++ requirements/build.bash
@@ -8,6 +8,13 @@ make install
 cd ../..
 
 cd fcl
-cmake .
+
+# check if cmake is called cmake3 *sigh*
+if hash cmake3 2>/dev/null; then
+    cmake3 .
+else
+    cmake .
+fi
+
 make -j4
 make install

c26c2fd50da9db9cb7592bf2f0fa3ab8f0bde2e9
Michael Dawson-Haggerty <mik3dh@gmail.com>
2017-10-17 23:00:35 -0400

    more cmake fun

--- requirements/build.bash
+++ requirements/build.bash
@@ -8,6 +8,13 @@ make install
 cd ../..
 
 cd fcl
-cmake .
+# try using cmake by location
+if [ -e /usr/bin/cmake3 ]; then
+    /usr/bin/cmake3 .
+elif [ -e /usr/bin/cmake28 ]; then
+    /usr/bin/cmake28 .
+else
+    cmake .
+fi
 make -j4
 make install

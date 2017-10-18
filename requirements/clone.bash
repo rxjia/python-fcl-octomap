a3395af79cff421677c02ea300edd02ac08496df
Michael Dawson-Haggerty <mik3dh@gmail.com>
2017-10-17 22:47:24 -0400

    add tar.gz eigen

--- requirements/clone.bash
+++ requirements/clone.bash
@@ -11,3 +11,7 @@ git clone https://github.com/flexible-collision-library/fcl.git
 cd fcl
 git pull
 git checkout 22f375f333beccc10c527974cef96784f0841649
+
+# get eigen
+curl -OL https://github.com/RLovelett/eigen/archive/3.3.4.tar.gz
+tar -zxvf 3.3.4.tar.gz

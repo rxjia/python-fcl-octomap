ad480880a95d3a701eb352bb9528d40768fa70c8
mikedh <mik3dh@gmail.com>
2017-10-20 00:16:50 -0400

    add cmake to path

--- requirements/install_cmake.bash
+++ requirements/install_cmake.bash
@@ -1,4 +1,4 @@
 # install cmake using their shell script
 
 curl -OL https://cmake.org/files/v3.10/cmake-3.10.0-rc3-Linux-x86_64.sh
-bash cmake-3.10.0-rc3-Linux-x86_64.sh --skip-license --prefix=/usr
+bash cmake-3.10.0-rc3-Linux-x86_64.sh --skip-license # --prefix=/usr
